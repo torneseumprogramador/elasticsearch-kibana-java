24aeab55d0a1ba82ce413401d9335b42f811d35f
Danilo Santos <danilo.aparecido.santos@gmail.com>
2022-04-29 14:29:16 -0300

    doc

--- src/main/java/br/com/elasticsearch/App.java
+++ src/main/java/br/com/elasticsearch/App.java
@@ -1,16 +1,50 @@
 package br.com.elasticsearch;
 
 import java.io.IOException;
+import java.util.List;
 
 import br.com.elasticsearch.Services.ElasticService;
 import br.com.elasticsearch.Services.RestService;
+import br.com.elasticsearch.models.Product;
 
 public class App 
 {
     public static void main( String[] args ) throws IOException
     {
         // RestService.post();
-        RestService.count();
+        // RestService.count();
+
+        // Product productOne = RestService.getOne();
+        // System.out.println("Nome: " + productOne.getName());
+        // System.out.println("Descrição: " + productOne.getDescription());
+
+        // List<Product> products = RestService.getList();
+        // for (Product product : products) {
+        //     System.out.println("===============================");
+        //     System.out.println("Nome: " + product.getName());
+        //     System.out.println("Descrição: " + product.getDescription());
+        //     System.out.println("===============================");
+        // }
+
+        listaPaginada(1);
+
         // ElasticService.get();
+    }
+
+    private static void listaPaginada(Integer pg) throws IOException {
+        List<Product> products = RestService.getPaggedList(pg);
+        if(products.size() > 0){
+            System.out.println("===============[Pagina: " + pg + "]================");
+
+            for (Product product : products) {
+                System.out.println("===============================");
+                System.out.println("Nome: " + product.getName());
+                System.out.println("Descrição: " + product.getDescription());
+                System.out.println("===============================");
+            }
+
+            listaPaginada(pg+1);
+        }
+        
     }
 }
--- src/main/java/br/com/elasticsearch/Services/RestService.java
+++ src/main/java/br/com/elasticsearch/Services/RestService.java
@@ -1,9 +1,12 @@
 package br.com.elasticsearch.Services;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.TimeZone;
 
 import com.google.gson.Gson;
+import com.google.gson.internal.LinkedTreeMap;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.HttpGet;
@@ -13,6 +16,8 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
 
+import br.com.elasticsearch.models.ElasticRequest;
+import br.com.elasticsearch.models.ElasticRequestList;
 import br.com.elasticsearch.models.Product;
 
 public class RestService {
@@ -45,5 +50,77 @@ public class RestService {
         var response = client.execute(get);
         String responseAsString = EntityUtils.toString(response.getEntity());
         System.out.println(responseAsString);
+    }
+
+    public static Product getOne() throws IOException
+    {
+        HttpGet get = new HttpGet("http://localhost:9200/products/_doc/1");
+        HttpClientBuilder clientBuilder = HttpClientBuilder.create();
+        CloseableHttpClient client = clientBuilder.build();
+        get.addHeader("Content-Type", "application/json");
+        get.addHeader("Accept","text/plain");
+        var response = client.execute(get);
+        
+        String responseAsString = EntityUtils.toString(response.getEntity());
+
+        var elasticRequest = new Gson().fromJson(responseAsString, ElasticRequest.class);
+        
+        var product = new Gson().fromJson(new Gson().toJson(elasticRequest.get_source()), Product.class);
+
+        return product;
+    }
+
+    public static List<Product> getList() throws IOException
+    {
+        HttpGet get = new HttpGet("http://localhost:9200/products/_search");
+        HttpClientBuilder clientBuilder = HttpClientBuilder.create();
+        CloseableHttpClient client = clientBuilder.build();
+        get.addHeader("Content-Type", "application/json");
+        get.addHeader("Accept","text/plain");
+        var response = client.execute(get);
+        
+        String responseAsString = EntityUtils.toString(response.getEntity());
+
+        var elasticRequestList = new Gson().fromJson(responseAsString, ElasticRequestList.class);
+        
+        List<Product> products = new ArrayList<>();
+
+        var hits = (List<LinkedTreeMap>)((LinkedTreeMap)elasticRequestList.get_hits()).get("hits");
+        
+        for (var productUnmapped : hits) {
+            var product_json = new Gson().toJson(productUnmapped.get("_source"));
+            var product = new Gson().fromJson(product_json, Product.class);
+            products.add(product);
+        }
+
+        return products;
+    }
+
+    public static List<Product> getPaggedList(Integer pg) throws IOException
+    {
+        HttpGet get = new HttpGet("http://localhost:9200/products/_search?size=10&from=" + (pg-1));
+        HttpClientBuilder clientBuilder = HttpClientBuilder.create();
+        CloseableHttpClient client = clientBuilder.build();
+        get.addHeader("Content-Type", "application/json");
+        get.addHeader("Accept","text/plain");
+        var response = client.execute(get);
+        
+        String responseAsString = EntityUtils.toString(response.getEntity());
+
+        var elasticRequestList = new Gson().fromJson(responseAsString, ElasticRequestList.class);
+        
+        List<Product> products = new ArrayList<>();
+
+        var hits = (List<LinkedTreeMap>)((LinkedTreeMap)elasticRequestList.get_hits()).get("hits");
+
+        if(hits.size() < 1) return products;
+        
+        for (var productUnmapped : hits) {
+            var product_json = new Gson().toJson(productUnmapped.get("_source"));
+            var product = new Gson().fromJson(product_json, Product.class);
+            products.add(product);
+        }
+
+        return products;
     }
 }
