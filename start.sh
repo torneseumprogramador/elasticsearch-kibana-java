18c39c58aa821869a9e0061b3291a55ce195718e
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2022-05-01 08:45:50 -0300

    removendo lixo

--- start.sh
+++ start.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-export CLASSPATH=target/elasticsearch-1.0-SNAPSHOT.jar
 export className=App
 echo "## Running $className..."
-shift
 mvn exec:java -Dexec.mainClass="br.com.elasticsearch.$className"
